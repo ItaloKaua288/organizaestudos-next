5e163ee3bbb294f026c9aa5c542efb7b25d3dbbc
ItaloKaua288 <italokaua177@gmail.com>
2026-07-14 15:32:06 -0300

    feat: add basic navbar using shadcn components

---
+++ frontend/src/components/nav-menu.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import type { ComponentProps } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export const NavMenu = (props: ComponentProps<typeof NavigationMenu>) => (
+  <NavigationMenu {...props}>
+    <NavigationMenuList className="data-[orientation=vertical]:-ms-2 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:justify-start">
+      <NavigationMenuItem>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href="#" />}>Dashboard</NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href="#" />}>Matérias</NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href="#" />}>Revisões</NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()} render={<Link href="#" />}>Notas</NavigationMenuLink>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
+);
