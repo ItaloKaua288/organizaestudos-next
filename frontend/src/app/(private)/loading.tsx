7bf7d5c215bdaca23227e53983eae86f7f6fa61e
ItaloKaua288 <italokaua177@gmail.com>
2026-08-08 20:24:43 -0300

    style: loading bar height and color

--- frontend/src/app/(private)/loading.tsx
+++ frontend/src/app/(private)/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="fixed left-0 right-0 top-18 z-100 h-1 overflow-hidden">
-      <div className="h-full w-1/3 animate-[loading_1s_ease-in-out_infinite] bg-blue-600" />
+    <div className="fixed left-0 right-0 top-18 z-100 h-0.5 overflow-hidden">
+      <div className="h-full w-1/3 animate-[loading_1s_ease-in-out_infinite] bg-primary" />
     </div>
   );
 }
