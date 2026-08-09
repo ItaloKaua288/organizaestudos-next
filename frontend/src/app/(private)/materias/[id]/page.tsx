8995d8a58eff022bb79c4c3e253cd8c1479ae890
kaiky nonato <kaikynonato288br@gmail.com>
2026-08-08 22:30:26 -0300

    feat: implementation of new features on the subject details page

--- frontend/src/app/(private)/materias/[id]/page.tsx
+++ frontend/src/app/(private)/materias/[id]/page.tsx
@@ -3,11 +3,13 @@ import { OptionsNoteCard } from "@/components/options-note-card";
 import ProgressLabelDemo from "@/components/progress-label";
 import { ReviewSection } from "@/components/reviewSection";
 import ScrambleText from "@/components/scramble-text";
+import { Badge } from "@/components/ui/badge";
+import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getTodayBR } from "@/lib/date";
 import { getDetailSubject } from "@/services/subjects.service";
 import { Topic } from "@/types/topic";
-import { CheckCircle2, Clock } from "lucide-react";
+import { CalendarClock, CheckCircle2, Clock } from "lucide-react";
 import Link from "next/link";
 import { CreateTopicForm } from "./components/create-topic-form";
 import { TopicList } from "./components/topic-list";
@@ -124,67 +126,96 @@ export default async function DetailMateriaPage({
 
                 <div className="flex gap-8 border-b-2">
                     <Link href="?tab=assuntos" className={tab === 'assuntos' ? activeTabStyle : inactiveTabStyle}>
-                        Assuntos
+                        Assuntos <Badge className="px-1 py-2.5" variant="secondary">{subject.topics?.length}</Badge>
                     </Link>
                     <Link href="?tab=notas" className={tab === 'notas' ? activeTabStyle : inactiveTabStyle}>
-                        Notas
+                        Notas <Badge className="px-1 py-2.5" variant="secondary">{subject.notes?.length}</Badge>
                     </Link>
                     <Link href="?tab=revisoes" className={tab === 'revisoes' ? activeTabStyle : inactiveTabStyle}>
-                        Revisões
+                        Revisões <Badge className="px-1 py-2.5" variant="secondary">{allReviews.filter(r => !r.concluded).length}</Badge> 
                     </Link>
                 </div>
 
-                {tab === 'assuntos' && (
-                    <div className="animate-in fade-in slide-in-from-bottom-2 duration-300 bg-card p-2 rounded-sm border">
-                        <TopicList sortedTopics={sortedTopics} subject={subject} />
-                        <CreateTopicForm subjectId={subject.id} />
-                    </div>
-                )}
-
-                {tab === 'notas' && (
-                    <div className="animate-in fade-in slide-in-from-bottom-2 duration-300 space-y-4">
-                        <div className="flex items-center justify-between mb-4">
-                            <h2 className="text-sm font-semibold text-zinc-400 uppercase tracking-wider">Suas Notas</h2>
-                            <CreateNoteDialog subjectId={id} />
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+                    {tab === 'assuntos' && (
+                        < div className="animate-in fade-in slide-in-from-bottom-2 duration-300 bg-card p-2 rounded-lg border col-span-1 md:col-span-3">
+                            <TopicList sortedTopics={sortedTopics} subject={subject} />
+                            <CreateTopicForm subjectId={subject.id} />
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                            {sortedNotes && sortedNotes.length > 0 ? (
-                                sortedNotes.map(note => (
-                                    <Card key={note.id} className="flex flex-col justify-between">
-                                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 gap-2">
-                                            <CardTitle className="truncate text-base font-semibold" title={note.title}>
-                                                {note.title}
-                                            </CardTitle>
-                                            <OptionsNoteCard key={note.id} subjectId={subject.id} note={note}></OptionsNoteCard>
-                                        </CardHeader>
+                    )}
 
-                                        <CardContent
-                                            className="prose prose-sm dark:prose-invert max-w-none pt-2 max-h-150 overflow-y-auto"
-                                            dangerouslySetInnerHTML={{ __html: note.content }}
-                                        />
-                                    </Card>
-                                ))
-                            ) : (
-                                <p className="text-sm text-zinc-500 col-span-3">Nenhuma nota encontrada para esta matéria.</p>
-                            )}
+                    {tab === 'notas' && (
+                        < div className="animate-in fade-in slide-in-from-bottom-2 duration-300 space-y-4 col-span-1 md:col-span-3">
+                            <div className="flex items-center justify-end mb-4">
+                                <CreateNoteDialog subjectId={id} />
+                            </div>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                                {sortedNotes && sortedNotes.length > 0 ? (
+                                    sortedNotes.map(note => (
+                                        <Card key={note.id} className="flex flex-col justify-between">
+                                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 gap-3">
+                                                <CardTitle className="truncate text-base font-semibold" title={note.title}>
+                                                    {note.title}
+                                                </CardTitle>
+                                                <OptionsNoteCard key={note.id} subjectId={subject.id} note={note}></OptionsNoteCard>
+                                            </CardHeader>
+
+                                            <CardContent
+                                                className="prose prose-sm dark:prose-invert max-w-none pt-2 max-h-150 overflow-y-auto"
+                                                dangerouslySetInnerHTML={{ __html: note.content }}
+                                            />
+                                        </Card>
+                                    ))
+                                ) : (
+                                    <p className="text-sm text-zinc-500 col-span-1 md:col-span-3">Nenhuma nota encontrada para esta matéria.</p>
+                                )}
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
 
-                {tab === 'revisoes' && (
-                    <div className="animate-in fade-in slide-in-from-bottom-2 duration-300 w-full flex flex-col gap-3">
-                        {sections.map((sec, index) => (
-                            <ReviewSection
-                                key={index}
-                                title={sec.title}
-                                reviewIndex={sec.reviewIndex!}
-                                icon={sec.icon}
-                                topics={sec.topics}
-                            />
-                        ))}
+                    {tab === 'revisoes' && (
+                        < div className="animate-in fade-in slide-in-from-bottom-2 duration-300 w-full flex flex-col gap-3 col-span-1 md:col-span-3">
+                            {sections.map((sec, index) => (
+                                <ReviewSection
+                                    key={index}
+                                    title={sec.title}
+                                    reviewIndex={sec.reviewIndex!}
+                                    icon={sec.icon}
+                                    topics={sec.topics}
+                                />
+                            ))}
+                        </div>
+                    )
+                    }
+
+                    <div className="flex flex-col gap-4 col-span-1 md:pr-2">
+                        <Card className="introduction-card w-full" style={{ animationDelay: `${1 * 150}ms` }} >
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-base font-semibold flex items-center justify-between gap-2">Cronograma <CalendarClock /></CardTitle>
+                                <span className="text-xs text-input">Dias da semana em que a matéria está vinculada no cronograma</span>
+                            </CardHeader>
+                            <CardContent className="flex gap-4 justify-center pt-0">
+                                <Badge className="p-3.5" variant={"secondary"}>Segunda-feira</Badge>
+                                <Badge className="p-3.5" variant={"secondary"}>Quarta-feira</Badge>
+                                <Badge className="p-3.5" variant={"secondary"}>Sexta-feira</Badge>
+                            </CardContent>
+                        </Card>
+                        <Card className="introduction-card w-full" style={{ animationDelay: `${2 * 150}ms` }} >
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-base font-semibold">Calendário</CardTitle>
+                            </CardHeader>
+                            <CardContent className="flex justify-center pt-0">
+                                <Calendar
+                                    className="rounded-xl border p-3 max-w-65 w-full"
+                                    classNames={{
+                                        today: "rounded-md bg-primary text-primary-foreground font-semibold",
+                                    }}
+                                />
+                            </CardContent>
+                        </Card>
                     </div>
-                )}
-            </section>
-        </main>
+                </div >
+            </section >
+        </main >
     );
 }
